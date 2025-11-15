e8a22b1cc13e42ecf555c1e766cfe408e2c0449f
Pau Torres <pautoper31@gmail.com>
2025-11-15 12:56:37 +0100

    fix: remove unnecessary Vite configuration from Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'], 
+  modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
-  vite: {
-    build: {
-       minify: false,
-    }
-  }, 
+  app: {
+    baseURL: '/gm-sprite-mesh/'
+  },
   ssr: false,
 })
